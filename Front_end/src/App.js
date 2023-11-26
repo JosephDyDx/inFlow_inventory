f30308c9b83d27e7ee19e229e0f19bcbfbfb3f9c
peterakinnawo <97162607+peterakinnawo@users.noreply.github.com>
2023-11-26 21:52:53 +0100

    Major Change
    Made Major Changes to the Index and made it the startup page

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -1,19 +1,11 @@
-import React from "react";
-import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
-import PasswordReset from "./pages/ForgotPassword/PasswordReset";
-import PhoneOtp from "./pages/OTP/PhoneOtp";
-import Product from "./pages/Products/Product";
+import Navbar from "./components/NavBar/navbar";
+import Intro from "./pages/Intro/intro";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/passwordreset" element={<PasswordReset />} />
-        <Route path="/phoneotp" element={<PhoneOtp/>}/>
-        <Route path="/product" element={<Product/>} />
-      </Routes>
+        <Navbar />
+        <Intro />
     </div>
   );
 }
