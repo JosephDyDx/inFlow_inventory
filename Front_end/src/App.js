523927f2d89304d606db645fba214a698a1eff27
ol109 <99981267+ol109@users.noreply.github.com>
2024-01-12 14:22:07 +0100

    made changes to the forgot password route

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<ForgotPassword />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/passwordreset" element={<PasswordReset />} />
         <Route path="/phoneotp" element={<PhoneOtp />} />
         <Route path="/product" element={<Product />} />
